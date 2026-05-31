6a5b2b01a6558aa6af441cb781baa060d58bbbe2
mukesh1237899-hash <mukesh1237899@gmail.com>
2026-05-31 23:58:51 +0530

    Update program3.c

--- Day-1/program3.c
+++ Day-1/program3.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
-{
-    int n;
+{//opening main
+    int n;//initialising
     printf("enter number");
-    scanf("%d",&n);
+    scanf("%d",&n);//store input in variables
     for(int i=1;i<=n;i++)
-    {
+    {//opening for loop
         if(n%i==0)
         printf("%d\n",i);
         else
         continue;
-    }
+    }//closing for loop
     return 0;
-}
+}//closing main
