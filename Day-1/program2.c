db7ec123fce957ca7b28bbc3bd1f377f0f3f8925
mukesh1237899-hash <mukesh1237899@gmail.com>
2026-05-31 23:57:23 +0530

    Update program2.c

--- Day-1/program2.c
+++ Day-1/program2.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
-{
-    int n,m=1;
+{//opening main
+    int n,m=1;//initialising variables
     printf("enter number");
     scanf("%d",&n);
     for(int i=1;i<=10;i++)
-    {
+    {//opening for loop
         m=n*i;
-         printf("%d\n",m);
-    }
+         printf("%d\n",m);//calculating multiples of number
+    }//closing for loop
     return 0;
-}
+}//closing main
