a679203d34443a0d3830683d6c4660b62e832aed
mukesh1237899-hash <mukesh1237899@gmail.com>
2026-06-01 00:04:36 +0530

    Update program4.c

--- Day-1/program4.c
+++ Day-1/program4.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-int main(){
-    int c=0,n;
+int main()
+{//opening main
+    int c=0,n;//initializing variables
     printf("enter number");
-    scanf("%d",&n);
+    scanf("%d",&n);//store input in variable
     while (n!=0)
-    {
+    {//opening while loop
      n=n/10;
-     c++;   /* code */
-    }
+     c++;
+    }//closing while loop
   printf("%d",c);
   return 0;  
-}
+}//closing main
